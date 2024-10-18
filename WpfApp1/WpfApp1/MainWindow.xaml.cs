8646e54175f2323c80da3795d53721e0beac0275
DiluxX <gostev.elist.ru@gmail.com>
2024-10-18 13:50:20 +0500

    does wpf file and answer a question of him

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (save.ShowDialog() == true)
+            {
+                string filePath = save.FileName;
+                File.WriteAllText(filePath, textBox.Text); // Сохранение текста в файл
+                MessageBox.Show("Текст успешно сохранен!");
+            }
+            else
+            {
+                MessageBox.Show("Сохранение отменено");
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Изменяем размер шрифта текстового поля в зависимости от значения слайдера
+            textBox.FontSize = e.NewValue;
+        }
+    }
+}
